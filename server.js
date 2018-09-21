4b03d22ec5cfc975c993a5f2264091584909e34b
jbweee <bui.just@gmail.com>
2018-09-20 21:44:30 -0700

    Using S3 links

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(parser.urlencoded({extended: true}));
 app.use('/api', router);
 
 app.use(morgan('dev'));
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log('STARKLOUD UP AND RUNNING ON PORT ', PORT);
